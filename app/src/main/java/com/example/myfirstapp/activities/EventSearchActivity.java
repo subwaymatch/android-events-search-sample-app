ea04e0fc41599fdccdeae3aaff2c61b4c704ed88
Ye Joo Park <subwaymatch@gmail.com>
2018-11-28 08:44:36 -0800

    Search form layout

--- app/src/main/java/com/example/myfirstapp/activities/EventSearchActivity.java
+++ app/src/main/java/com/example/myfirstapp/activities/EventSearchActivity.java
@@ -1,27 +1,21 @@
 package com.example.myfirstapp.activities;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.fragments.SearchFormFragment;
 
 public class EventSearchActivity extends AppCompatActivity {
+	private static final String TAG = "EventSearchActivity";
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -60,45 +54,11 @@ public class EventSearchActivity extends AppCompatActivity {
 	}
 
 	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		public PlaceholderFragment() {
-		}
-
-		/**
-		 * Returns a new instance of this fragment for the given section
-		 * number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-								 Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_event_search, container, false);
-			TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-			textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-
-	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
 	 */
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+		private SearchFormFragment searchFormFragment;
 
 		public SectionsPagerAdapter(FragmentManager fm) {
 			super(fm);
@@ -106,14 +66,21 @@ public class EventSearchActivity extends AppCompatActivity {
 
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class below).
-			return PlaceholderFragment.newInstance(position + 1);
-		}
+			if (position == 0) {
+				searchFormFragment = SearchFormFragment.newInstance("hello", "world");
+				return searchFormFragment;
+			}
 
+			else {
+				// getItem is called to instantiate the fragment for the given page.
+				// Return a PlaceholderFragment (defined as a static inner class below).
+				return SearchFormFragment.newInstance("hello", "world");
+			}
+		}
+		
 		@Override
 		public int getCount() {
-			// Show 3 total pages.
+			// Show 2 total pages.
 			return 3;
 		}
 	}
